3e63149ac7b78feca20ab0e1d889f35d66810a32
SophieJanson <sophie@sophiejanson.com>
2018-07-27 19:22:28 +0200

    Improved createMerge function

--- src/matches/controller.ts
+++ src/matches/controller.ts
@@ -12,9 +12,16 @@ import {
 } from "routing-controllers";
 import Match from "./entity";
 import User from "../users/entity";
-import { getConnection } from "../../node_modules/typeorm";
-//import User from "../users/entity";
-//import { ConnectionManager } from "../../node_modules/typeorm";
+// import { getConnection } from "../../node_modules/typeorm";
+// import Activity from "../activities/entity";
+// import { ConnectionManager } from "../../node_modules/typeorm";
+
+interface MatchRequest {
+  users: User[],
+  activities: string[],
+  categories: string[],
+  status: string
+}
 
 @JsonController()
 export default class MatchController {
@@ -27,20 +34,8 @@ export default class MatchController {
   @Authorized()
   @Post("/matches")
   @HttpCode(201)
-  async createMatch(
-    @Body() match: Match) {
-      if(!match.users) throw new BadRequestError("No users specified")
-      if(!match.activities) throw new BadRequestError("No activity specified")
-      if(!match.categories) throw new BadRequestError("No category specified")
-      if(!match.status) throw new BadRequestError("No status specified")
-    // const usertwo = await User.findOne(2)
-    // if(!usertwo) throw NotFoundError
-    // const newMatch = new Match()
-    // newMatch.activities = ["swimming"]
-    // newMatch.categories = ["learn"]
-    // newMatch.status = "pending"
-    // newMatch.users = [user, usertwo]
-
-    return match.save();
+  async createMatch(@Body() match: MatchRequest) {
+    const newMatch = new Match()
+    return Match.merge(newMatch, match).save();
   }
 }
