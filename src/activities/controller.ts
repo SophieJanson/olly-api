f3e46ad0bd2886dba0e8b24437cfd4ba8f053788
SophieJanson <sophie@sophiejanson.com>
2018-07-27 11:27:32 +0200

    added activity endpoint

--- src/activities/controller.ts
+++ src/activities/controller.ts
@@ -1,4 +1,12 @@
-import { JsonController } from "routing-controllers";
+import { JsonController, Get } from "routing-controllers";
+import Activity from "./entity";
 
 @JsonController()
-export default class ActivityController {}
+export default class ActivityController {
+  @Get('/activities')
+  async getActivities() {
+    return await {
+      activities: Activity.find()
+    }
+  }
+}
