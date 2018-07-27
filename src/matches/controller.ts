098297dbcafffbe7c04a5693985e4c6c81e5b734
“riannevanson“ <riannevanson@gmail.comssh-keygen -t rsa -C riannevanson@gmail.com>
2018-07-27 11:03:25 +0200

    made extra entity matchedUser

--- src/matches/controller.ts
+++ src/matches/controller.ts
@@ -1,10 +1,36 @@
-import { JsonController, Get, Param } from "routing-controllers";
+import {
+  JsonController,
+  Get,
+  Param,
+  Authorized,
+  Post,
+  CurrentUser,
+  HttpCode
+} from "routing-controllers";
 import Match from "./entity";
+import User from "../users/entity";
 
 @JsonController()
 export default class MatchController {
+  @Authorized()
   @Get("/matches/:matchId")
   getMatch(@Param("matchId") matchId: number) {
     return Match.findOne(matchId);
+  }
+
+  @Authorized()
+  @Post("/matches")
+  @HttpCode(201)
+  async createMatch(@CurrentUser() user: User) {
+    const entity = await Match.create().save();
+
+    await MatchedPerson.create({
+      match: entity,
+      user
+    }).save();
+
+    const match = await Match.findOne(entity.id);
+
+    return match;
   }
 }
--- src/matches/entity.ts
+++ src/matches/entity.ts
@@ -4,34 +4,48 @@ import {
   PrimaryGeneratedColumn,
   Column,
   ManyToMany,
-  OneToMany
+  OneToMany,
+  Index,
+  ManyToOne
 } from "typeorm";
 import User from "./entity";
-import WeeklyUpdate from "../weeklyUpdates/entity"
-import FollowUp from "../followups/entity"
+import WeeklyUpdate from "../weeklyUpdates/entity";
+import FollowUp from "../followups/entity";
 
 @Entity()
 export default class Match extends BaseEntity {
-  
-	@PrimaryGeneratedColumn() 
-	id?: number;
+  @PrimaryGeneratedColumn() id?: number;
 
-	@Column("text") 
-	categories: string[];
+  @Column("text") categories: string[];
 
-	@Column("text") 
-	activities: string[];
+  @Column("text") activities: string[];
 
-	@Column("text") 
-	status: string;
-	
-	@ManyToMany(_ => User, user => user.id)
-	@Column({type: 'integer', array: true})
-	users: number[];
+  @Column("text") status: string;
 
-	@OneToMany(_ => WeeklyUpdate, weeklyUpdate => weeklyUpdate.match) 
-  	weeklyUpdate: number[]
+  @ManyToMany(_ => User, user => user.id)
+  @Column({ type: "integer", array: true })
+  users: number[];
 
-	@OneToMany(_ => FollowUp, followUp => followUp.match) 
-  		followUps: number[]
+  @OneToMany(_ => WeeklyUpdate, weeklyUpdate => weeklyUpdate.match)
+  weeklyUpdate: number[];
+
+  @OneToMany(_ => FollowUp, followUp => followUp.match)
+  followUps: number[];
+
+  @OneToMany(_ => MatchedUser, matchedUser => matchedUser.match)
+  matchedUser: number[];
+}
+
+@Entity()
+@Index(["match", "user"], { unique: true })
+export class MatchedUser extends BaseEntity {
+  @PrimaryGeneratedColumn() id?: number;
+
+  @ManyToOne(_ => User, user => user.matchedUsers)
+  user: User;
+
+  @ManyToOne(_ => Match, match => match.matchedUsers)
+  match: Match;
+
+  @Column() userId: number;
 }
