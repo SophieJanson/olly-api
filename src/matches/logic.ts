28d87c48fbde326ed598ff51d238b2ee40209c81
tasos <tasos.krikonis@gmail.com>
2018-07-31 14:56:07 +0200

    bla

--- src/matches/logic.ts
+++ src/matches/logic.ts
@@ -1,69 +1,78 @@
-import User from "../users/entity";
-import WeeklyUpdate from "../weeklyUpdates/entity";
-import { getRepository } from "../../node_modules/typeorm";
-//import Activity from "../activities/entity";
-//import { getRepository } from "typeorm";
-//import Activity from "../activities/entity";
+import User from '../users/entity'
+import WeeklyUpdate from '../weeklyUpdates/entity'
+import { getRepository } from 'typeorm';
 
-export async function getDepartment(inputDepartment) {
-  let resultDepartment = async () => {
-    //  console.log(cat, "cat 1");
-    return await User.find({
-      select: ["department", "id", "firstName", "lastName"],
 
-      where: {
-        department: inputDepartment
-      }
-    });
-    console.log("cat dep");
-  };
-  return await resultDepartment();
-}
-
-export async function getActivity(inputActivities) {
-  let resultActivity = async () => {
-    console.log(inputActivities);
-    return await getRepository(WeeklyUpdate)
-      .createQueryBuilder("weeklyupdate")
-      .leftJoinAndSelect("weeklyupdate.activityId", "activity")
-      .where("activity.activityName = :inputActivities")
-      //  .andWhere('week = 1')
-      .setParameter("inputActivities", inputActivities)
-      .getMany();
-    console.log(inputActivities);
-  };
-
-  return await resultActivity();
-}
 
 export async function getCategory(inputCategory) {
-  let resultCat = async () => {
-    //  console.log(cat, "cat 1");
-    return await WeeklyUpdate.find({
-      select: ["category", "id"],
-      relations: ["user"],
+    let cat = async () => {
+      //  console.log(cat, "cat 1");
+      return await WeeklyUpdate.find({
+        select: ["category", "id"],
+        relations: ["user"],
+        where: {
+          category: inputCategory
+        }
+      });
+      console.log("cat 2");
+    };
+    return await cat();
+  }
 
-      where: {
-        category: inputCategory
-      }
-    });
-    console.log("cat 2");
-  };
-  return await resultCat();
+   export async function getDepartment(inputDepartment) {
+    let catDep = async () => {
+      //  console.log(cat, "cat 1");
+      return await User.find({
+        select: ["department", "id", "firstName", "lastName"],
+        where: {
+          department: inputDepartment
+        }
+      });
+      console.log("cat dep");
+    };
+    return await catDep();
+  }
+
+function getActivity(activity) {
+    let activities = async () => { 
+        return await getRepository(WeeklyUpdate)
+        .createQueryBuilder('weeklyUpdate')
+        .where("weeklyUpdate_activity = :weeklyUpdateActivity", {
+            weeklyActivity: activity
+          })
+        .innerJoinAndSelect('weeklyUpdate.activity', 'activity')
+        .getMany()
+    }
+    return activities
 }
+
+
 
 export default function algolly(department, category, activity) {
-  if (department !== null) {
-    getDepartment(department);
-  }
-
-  if (category !== null) {
-    getCategory(category);
-  }
-
-  if (activity !== null) {
-    getActivity(activity);
-  }
+    
+    const  matchesResult = [] as any[];
+    
+        if (department !== null) {
+        
+            let departments = async () => { 
+                return await getRepository(User)
+                    .createQueryBuilder("user")
+                    .where("user_department = :userDepartment", { userDepartment: department })
+                    .getMany()
+            }
+                matchesResult.push(departments())
+            
+                return matchesResult
+            
+            }
+            
+            if (category !== null) {
+                getCategory(category)
+            }
+            
+            if (activity !== null) {
+                getActivity(activity)
+            }
+        
+    
 }
-
-//export const logWeekly = () => WeeklyUpdate.find();
