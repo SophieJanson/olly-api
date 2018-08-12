2b4617ce8977d2452b69ef18232e9a4dcf93ab6f
SophieJanson <sophie@sophiejanson.com>
2018-08-12 15:08:46 +0200

    Made sure to return challenge if present

--- src/slackbot/controller.ts
+++ src/slackbot/controller.ts
@@ -52,6 +52,9 @@ export default class SlackbotController {
 		@HttpCode(200)
 		@Body() body: any
 	) {
+		//Slack needs this to validate the request URL. 
+		if(body.challenge) return body.challenge
+		
 		if(!body.event || body.event.type !== 'message' || body.event.bot_id || !body.event.text) return "Error"
 		
 		if(body.event.text.includes('goals')) {
@@ -73,7 +76,7 @@ export default class SlackbotController {
 			return ""
 		}
 
-		return body.challenge ? body.challenge : "Hello"
+		return ""
 	}
 
   @Post('/slacktest')
